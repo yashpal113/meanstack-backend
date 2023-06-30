f887031f7699285c3a45c6504c5fde2ef13deb85
yashpal113 <137606006+yashpal113@users.noreply.github.com>
2023-06-30 11:56:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-const express=require("express"); const app=express(); app.listen(3000);
-const cors=require("cors");app.use(cors()); const bodyParser=require("body-parser");
-app.use(bodyParser.urlencoded({extended:false})); app.use(express.json());
+const express=require("express");
+const app=express();
+const port=process.env.port || 3000 ;
+app.listen(port, ()=>{ console.log("port : "+ port)});
 
-app.get("/", (req,res)=>{     res.send("Hello Acro"); });
+const cors=require("cors");
+app.use(cors());
+app.use(express.json());
+const bodyParser=require("body-parser");
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.get("/", (req,res)=>{
+    res.json({"message":{"data": req.query}});
+})
+app.post("/", (req,res)=>{
+    res.json({"message":{"data": req.body}});
+})
